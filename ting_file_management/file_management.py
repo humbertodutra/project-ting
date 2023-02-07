b58809240d92cc600c959c6d3268dc16d3bfbe38
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-07 16:20:29 -0300

    exer 1,2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                contents = file.read()
+                data = contents.split("\n")
+                print(contents)
+                return data
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print("Formato inválido", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,9 @@
+# import os
+# from ting_file_management.queue import Queue
+# from ting_file_management.file_management import text_importer
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-
 
 def remove(instance):
     """Aqui irá sua implementação"""
