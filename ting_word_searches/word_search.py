a4ad20a9ed59ceb8d1495b85c3356b3e8ec8e5e8
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-07 19:54:16 -0300

    exercise 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
-def exists_word(word, instance):
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
     """Aqui irá sua implementação"""
+    word_search = list()
+    data = instance.data
+
+    for file in data:
+        found_word = ()
+        for i, line in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                found_word = (file['nome_do_arquivo'], i)
+                found_word.append({"linha": i + 1})
+        if len(found_word) > 0:
+            word_search.append(
+                {"palavra": word,
+                 "arquivo": file["nome_do_arquivo"],
+                 "ocorrencias": found_word})
+    return word_search
 
 
 def search_by_word(word, instance):
