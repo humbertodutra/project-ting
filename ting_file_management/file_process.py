8a2db8fec365ef0b8c53e7a8b94cf663d6158ef2
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-02-07 18:52:15 -0300

    ?

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,14 +1,37 @@
-# import os
-# from ting_file_management.queue import Queue
-# from ting_file_management.file_management import text_importer
+import sys
 
-def process(path_file, instance):
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
     """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    result = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file
+    }
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+    instance.enqueue(result)
+    sys.stdout.write(f'{result}')
 
 
-def remove(instance):
+def remove(instance: Queue):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        sys.stdout.write('Não há elementos\n')
+        return None
+    remove_file = instance.dequeue()['nome_do_arquivo']
+    sys.stdout.write(f'Arquivo {remove_file} removido com sucesso\n')
 
 
-def file_metadata(instance, position):
+def file_metadata(instance: Queue, position):
     """Aqui irá sua implementação"""
+    try:
+        search = instance.search(position)
+        sys.stdout.write(f'{search}')
+    except IndexError:
+        sys.stderr.write('Posição inválida')
